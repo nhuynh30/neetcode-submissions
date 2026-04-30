a140a970c406e27ef5b56b69eab0c0424258e27a
nhuynh30 <quanhuynh364@gmail.com>
2026-04-30 18:01:31 -0400

    Add: design-twitter-feed - submission-1

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-1.py
@@ -0,0 +1,54 @@
+class Twitter:
+
+    def __init__(self):
+        self.followed = {}
+        self.tweet = {}
+        self.time = 0
+
+
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        if userId in self.tweet:
+            self.tweet[userId].append((-self.time, tweetId))
+        else:
+            self.tweet[userId] = [(-self.time, tweetId)]
+        self.time+=1
+        
+        
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        lst = set()
+        if userId in self.followed:
+            lst = self.followed[userId].copy()
+        lst.add(userId)
+
+        
+        x = []
+        for user in lst:
+            for tweet in self.tweet.get(user,[]):
+                heapq.heappush(x, tweet)
+        
+        res = []
+        while len(res)<10 and x:
+            time, tweetid = heapq.heappop(x)
+            res.append(tweetid)
+            
+        
+        return res
+
+            
+
+
+        
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.followed:
+            self.followed[followerId].add(followeeId)
+        else:
+            self.followed[followerId] = set()
+            self.followed[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.followed:
+            self.followed[followerId].discard(followeeId)
+        
